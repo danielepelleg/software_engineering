dd1aa0a8f50fe8b7daa7c45c9047165b9c45d587
BeleRicks11 <55739190+BeleRicks11@users.noreply.github.com>
2019-11-04 13:40:13 +0100

    Update Person.java
    - Fix the setHaschedPassword()
    Now a client could have null parameters.
    A client with null parameters is a not registered user and can only search wines

--- Assegnamento2/src/main/java/it/unipr/fava_pellegrini/Person.java
+++ Assegnamento2/src/main/java/it/unipr/fava_pellegrini/Person.java
@@ -69,15 +69,21 @@ public class Person{
      */
     public void setHashedPassword(String password) {
         try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(password.getBytes());
-            byte[] bytes = md.digest();
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            if(password == null){
+                this.hashed_password = null;
             }
-            this.hashed_password = sb.toString();
+            else {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(password.getBytes());
+                byte[] bytes = md.digest();
+                StringBuilder sb = new StringBuilder();
+                for(int i=0; i< bytes.length ;i++)
+                {
+                    sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+                }
+                this.hashed_password = sb.toString();
+            }
+
         }
         catch (NoSuchAlgorithmException e)
         {
