758ece35439dcfe35c2087aa5954a7a48bcec386
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2019-12-28 12:34:39 +0100

    Add Registration Form and RegistrationController Class

---
+++ Assegnamento4/src/main/java/Registration/RegistrationController.java
@@ -0,0 +1,55 @@
+package Registration;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private Button backLoginButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField surnameField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private CheckBox adminButton;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+}
