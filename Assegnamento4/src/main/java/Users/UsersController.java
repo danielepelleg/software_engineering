b0c08f722fd0fab52449ee9e5b218cdb562d0c6f
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2020-01-16 11:49:53 +0100

    Add Application Icon

--- Assegnamento4/src/main/java/Users/UsersController.java
+++ Assegnamento4/src/main/java/Users/UsersController.java
@@ -1,42 +1,53 @@
 package Users;
 
-package Activities;
-
+import AlertBox.WarningBox;
+import Database.DatabaseManager;
+import MenuMember.Subscription;
+import SportClub.Course;
+import SportClub.Session;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
 
 public class UsersController {
 
     @FXML
-    private Button subscribeButton;
+    private ComboBox<String> userComboBox;
 
     @FXML
-    private ComboBox<?> userComboBox;
+    private Button deleteButton;
 
     @FXML
-    private Button unsubscribeButton;
+    private Button addButton;
 
     @FXML
-    private ComboBox<?> comboBox;
-
-    @FXML
-    private Button loadButton;
+    private Button updateButton;
 
     @FXML
     private Button menuButton;
 
     @FXML
-    private TableView<?> subscriptionTable;
+    private TableView<Subscription> subscriptionTable;
 
     @FXML
-    private TableColumn<?, ?> courseColumn;
+    private TableColumn<String, Subscription> courseColumn;
 
     @FXML
-    private TableColumn<?, ?> subscriptionColumn;
+    private TableColumn<String, Subscription> subscriptionColumn;
 
     @FXML
     void backMenu(ActionEvent event) {
@@ -44,17 +55,18 @@ public class UsersController {
     }
 
     @FXML
-    void loadSubscriptionData(ActionEvent event) {
+    void addUser(ActionEvent event) {
 
     }
 
     @FXML
-    void subscribeMember(ActionEvent event) {
+    void deleteUser(ActionEvent event) {
 
     }
 
     @FXML
-    void unsubscribeMember(ActionEvent event) {
+    void updateUser(ActionEvent event) {
 
     }
+
 }
